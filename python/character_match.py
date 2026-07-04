662e1d62fc74279a6308fa0f38834aa9801702a7
KgitWH21 <kwhayden11@gmail.com>
2026-07-04 15:57:15 -0700

    exercise complete

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,21 @@
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	if len(string1) != len(string2):
+      return False
+    
+  char_counts = {}
+  
+  lower1 = string1.lower()
+  lower2 = string2.lower()
+  
+  for char in lower1:
+    char_counts[char] = char_counts.get(char, 0) + 1
+    
+  for char in lower2:
+    if not char_counts.get(char):
+      return False
+    
+    char_counts[char] -= 1
+  
+  return True
